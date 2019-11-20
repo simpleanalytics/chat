cf593df18f84d6e73c0a8d092a664c3ae8004aaf
Adriaan van Rossum <git@adriaan.io>
2019-11-20 14:24:48 +0700

    Use smaller image for my face

--- src/widget/default-configuration.js
+++ src/widget/default-configuration.js
@@ -1,19 +1,21 @@
 export const defaultConfiguration = {
-    titleClosed: 'Re-open chat',
-    titleOpen: 'Ask me anything',
-    closedStyle: 'button', // button or chat
-    closedChatAvatarUrl: '', // only used if closedStyle is set to 'chat'
-    cookieExpiration: 1, // in days. Once opened, closed chat title will be shown as button (when closedStyle is set to 'chat')
-    introMessage: 'Hello! How can we help?',
-    introMessageBalloon: 'Hello! How can we help?',
-    closedChatAvatarUrl: 'https://cdn.simpleanalytics.io/images/adriaan.jpg',
-    autoResponse: 'Looking for Adriaan or Şamil (this might take a minute)',
-    autoNoResponse: 'They don\'t seem to answer within 60 seconds. This chat is anonymous so please leave your email address so they can ' +
-    'contact you.',
-    placeholderText: 'Send a message...',
-    displayMessageTime: true,
-    mainColor: '#FF4F64',
-    alwaysUseFloatingButton: false,
-    desktopHeight: 450,
-    desktopWidth: 300
+  titleClosed: "Re-open chat",
+  titleOpen: "Ask me anything",
+  closedStyle: "button", // button or chat
+  closedChatAvatarUrl: "", // only used if closedStyle is set to 'chat'
+  cookieExpiration: 1, // in days. Once opened, closed chat title will be shown as button (when closedStyle is set to 'chat')
+  introMessage: "Hello! How can we help?",
+  introMessageBalloon: "Hello! How can we help?",
+  closedChatAvatarUrl:
+    "https://cdn.simpleanalytics.io/images/adriaan-small.jpg",
+  autoResponse: "Looking for Adriaan or Şamil (this might take a minute)",
+  autoNoResponse:
+    "They don't seem to answer within 60 seconds. This chat is anonymous so please leave your email address so they can " +
+    "contact you.",
+  placeholderText: "Send a message...",
+  displayMessageTime: true,
+  mainColor: "#FF4F64",
+  alwaysUseFloatingButton: false,
+  desktopHeight: 450,
+  desktopWidth: 300
 };
