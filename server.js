e2b608b6e06d733219ff1612664e7ab18485bcd3
Adriaan van Rossum <git@adriaan.io>
2020-09-02 23:19:01 +0200

    Listen for /hook without /${process.env.TELEGRAM_TOKEN}

--- server.js
+++ server.js
@@ -1,79 +1,86 @@
-const request = require('request');
+const request = require("request");
 // const cors = require('cors');
-const express = require('express');
-const bodyParser = require('body-parser');
+const express = require("express");
+const bodyParser = require("body-parser");
 const app = express();
-const http = require('http').Server(app);
-const io = require('socket.io')(http);
+const http = require("http").Server(app);
+const io = require("socket.io")(http);
 
-app.use(express.static('dist', {index: 'demo.html', maxage: '4h'}));
+app.use(express.static("dist", { index: "demo.html", maxage: "4h" }));
 app.use(bodyParser.json());
 
-const sessions = {}
-const buffer = {}
+const sessions = {};
+const buffer = {};
 // const historyIds = []
 
 // handle admin Telegram messages
-app.post(`/hook/${process.env.TELEGRAM_TOKEN}`, function(req, res) {
+app.post(`/hook`, function (req, res) {
   try {
     const message = req.body.message || req.body.channel_post;
     const chatId = process.env.TELEGRAM_CHAT_ID; // || message.chat.id;
-    let name = message.from.first_name || message.chat.first_name || 'Support';
-    const text = message.text || '';
+    let name = message.from.first_name || message.chat.first_name || "Support";
+    const text = message.text || "";
     const reply = message.reply_to_message;
 
-    console.log(JSON.stringify(req.body))
+    console.log(JSON.stringify(req.body));
 
-    if (/adriaan/gi.test(name)) name = 'Adriaan'
+    if (/adriaan/gi.test(name)) name = "Adriaan";
 
-    if (text.startsWith('/start')) {
+    if (text.startsWith("/start")) {
       // console.log('/start chatId ' + chatId);
-      sendTelegramMessage(chatId,
-        '*Welcome to Intergram* \n' +
-        'Your unique chat id is `' + chatId + '`\n' +
-        'Use it to link between the embedded chat and this telegram chat',
-        'Markdown');
+      sendTelegramMessage(
+        chatId,
+        "*Welcome to Intergram* \n" +
+          "Your unique chat id is `" +
+          chatId +
+          "`\n" +
+          "Use it to link between the embedded chat and this telegram chat",
+        "Markdown"
+      );
     } else if (reply) {
-      let replyText = reply.text || '';
-      let userId = replyText.split(':')[0];
-      const socketId = sessions[userId]
+      let replyText = reply.text || "";
+      let userId = replyText.split(":")[0];
+      const socketId = sessions[userId];
 
       if (socketId) {
-        io.to(socketId).emit(chatId + '-' + userId, { name, text, from: 'admin' });
+        io.to(socketId).emit(chatId + "-" + userId, {
+          name,
+          text,
+          from: "admin",
+        });
       } else {
         if (!buffer[userId]) {
           buffer[userId] = [];
         }
 
-        console.log('client not connected buffering message');
+        console.log("client not connected buffering message");
         buffer[userId].unshift({
           chatId,
           name,
           text,
-          from: 'admin',
-          adminName: name
-        })
+          from: "admin",
+          adminName: name,
+        });
       }
     } else if (text) {
       // io.emit(chatId, {name, text, from: 'admin'});
     }
   } catch (e) {
-    console.error('hook error', e, req.body);
+    console.error("hook error", e, req.body);
   }
   res.statusCode = 200;
   res.end();
 });
 
 // handle chat visitors websocket messages
-io.on('connection', function(socket) {
-
-  socket.on('register', function(registerMsg) {
-    console.log('on register')
+io.on("connection", function (socket) {
+  socket.on("register", function (registerMsg) {
+    console.log("on register");
     let userId = registerMsg.userId;
     let chatId = registerMsg.chatId;
     // let messageReceived = false;
 
-    sessions[userId] = socket.id
+    sessions[userId] = socket.id;
 
     // if (historyIds.indexOf(userId) > -1) {
     //   messageReceived = true;
@@ -86,52 +93,59 @@ io.on('connection', function(socket) {
       console.log(`sending ${buffered.length} buffered messages`);
       while (msg) {
         const { chatId, name, text, from, adminName } = msg;
-        io.to(socket.id).emit(chatId + '-' + userId, {
+        io.to(socket.id).emit(chatId + "-" + userId, {
           name,
           text,
           from,
-          adminName
+          adminName,
         });
         msg = buffered.pop();
       }
       delete buffer[userId];
     }
 
-    socket.on('message', function(msg) {
+    socket.on("message", function (msg) {
       // console.log('on message', msg)
       messageReceived = true;
       // if (msg.from !== 'bot') io.emit(chatId + '-' + userId, msg);
-      if (msg.from !== 'bot') io.to(socket.id).emit(chatId + '-' + userId, msg);
+      if (msg.from !== "bot") io.to(socket.id).emit(chatId + "-" + userId, msg);
       sendTelegramMessage(chatId, `${userId}: ${msg.text}`);
     });
 
-    socket.on('disconnect', function() {
+    socket.on("disconnect", function () {
       // console.log('on disconnect')
       // if (historyIds.indexOf(userId) === -1) historyIds.push(userId)
       // if (messageReceived) {
       //   sendTelegramMessage(chatId, `${userId}: has left`, null, true);
       // }
-      delete sessions[userId]
+      delete sessions[userId];
     });
   });
 });
 
-function sendTelegramMessage(chatId, text, parseMode, disableNotification = false) {
+function sendTelegramMessage(
+  chatId,
+  text,
+  parseMode,
+  disableNotification = false
+) {
   const data = {
     disable_notification: disableNotification,
     chat_id: chatId,
-    text
-  }
-  if (parseMode) data.parse_mode = parseMode
-
-  console.log(data)
+    text,
+  };
+  if (parseMode) data.parse_mode = parseMode;
 
   request
-    .post('https://api.telegram.org/bot' + process.env.TELEGRAM_TOKEN + '/sendMessage')
+    .post(
+      "https://api.telegram.org/bot" +
+        process.env.TELEGRAM_TOKEN +
+        "/sendMessage"
+    )
     .form(data)
-    .on('error', function(err) {
-      console.error(err)
-    })
+    .on("error", function (err) {
+      console.error(err);
+    });
 }
 
 // app.post('/usage-start', cors(), function(req, res) {
@@ -146,6 +160,6 @@ function sendTelegramMessage(chatId, text, parseMode, disableNotification = fals
 //   res.end();
 // });
 
-http.listen(process.env.PORT || 3001, function(){
-  console.log('listening on port:' + (process.env.PORT || 3001));
+http.listen(process.env.PORT || 3001, function () {
+  console.log("listening on port:" + (process.env.PORT || 3001));
 });
